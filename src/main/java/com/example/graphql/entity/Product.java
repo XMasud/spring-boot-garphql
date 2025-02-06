178109e5999d93e8f3cccf8a9c81e413ce7b4608
Masud Rana <masud.rana@apsissolutions.com>
2025-02-06 18:54:36 +0100

    DB config and Entity added

---
+++ src/main/java/com/example/graphql/entity/Product.java
@@ -0,0 +1,25 @@
+package com.example.graphql.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+}
